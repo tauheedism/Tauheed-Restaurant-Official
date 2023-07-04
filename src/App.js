e6621a580a6a0d5c7134ce316a88e8e634003a7d
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-07-04 13:10:30 +0530

    Adding summary to restaurant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Fragment}from 'react';
 import Header from './Components/Layout/Header'
+import MealsSummary from './Components/Layout/MealsSummary'
 
 const App = () => {
   return (
     <Fragment>
       <Header/>
+      <MealsSummary/>
     </Fragment>
   )
 }
