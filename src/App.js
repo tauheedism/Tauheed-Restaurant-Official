dab0c693596e176b2bcae4df44d1c77c7109f498
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-07-04 18:01:21 +0530

    Modal Overlay into the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React,{Fragment}from 'react';
 import Header from './Components/Layout/Header'
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 const App = () => {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
       </main>
     </Fragment>
+
   )
 }
 
---
+++ src/Components/Cart/Modal.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import classes from './Modal.module.css';
+import  ReactDOM  from 'react-dom';
+const Backdrop = props => {
+    return <div className={classes.backdrop}>{props.children}</div>
+}
+
+const ModalOverlay = props => {
+    return <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>
+}
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+    {ReactDOM.createPortal(<Backdrop />, document.getElementById('backdrop-root'))}
+    {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay> , document.getElementById('overlay-root'))}
+    </Fragment>
+  )
+}
+
+export default Modal
